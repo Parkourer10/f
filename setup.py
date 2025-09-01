40c36356824a09f16d44b87effe828d43809389e
Park <168292059+Parkourer10@users.noreply.github.com>
2025-09-01 15:12:23 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='FlightScrapper',
-    version='0.1.0',  # Update this with your version
-    author='Your Name',
-    author_email='your.email@example.com',
-    description='A FastAPI application that scrapes and tracks flight information',
+    version='0.1.0',
+    author='ahmadms1',
+    author_email='your.email@example.com', #CHANGEME
+    description='A FastAPI application that scrapes and tracks flight information from flightstats.com',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/your-username/FlightScrapper',  # Replace with your repository URL
+    url='https://github.com/ahmadms1/flight_scrapper.git',
     packages=find_packages(),
     install_requires=[
-        'fastapi==0.95.0',
-        'pydantic==1.10.7',
+        'fastapi>=0.100',
+        'pydantic>=2.0',
         'requests==2.28.1',
         'beautifulsoup4==4.11.1',
         'sqlalchemy==1.4.46',
@@ -25,16 +25,11 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',  # or whatever license you're using
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
     ],
     python_requires='>=3.7',
-    entry_points={
-        'console_scripts': [
-            'run_flight_scrapper=main:run',  # Assuming you have a function named 'run' in 'main.py'
-        ],
-    },
 )
